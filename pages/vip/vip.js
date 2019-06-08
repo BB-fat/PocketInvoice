e8909a5e8fab9d3842e1b418e5369a9ecc139c23
鞠欣诚 <bbfat3047@qq.com>
2019-06-08 14:24:29 +0800

    完成新的充值页面

--- pages/vip/vip.js
+++ pages/vip/vip.js
@@ -7,7 +7,38 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    chooseOne:0,
+    btn_pay: {
+      tapFun: 'pay',
+      text: "点击购买",
+      color: "#EF9E1E",
+      width: 620,
+      mode: "mid"
+    },
+    btn_120:{
+      count:120,
+      price:200,
+    },
+    btn_60:{
+      count:60,
+      price:100,
+    },
+    btn_35:{
+      count:35,
+      price:50,
+    },
+    btn_12:{
+      count:12,
+      price:20,
+    },
+    btn_6:{
+      count:6,
+      price:10,
+    },
+    btn_1:{
+      count:1,
+      price:2,
+    },
   },
 
 onLoad:function(){
@@ -38,4 +69,10 @@ onLoad:function(){
     })
   },
 
+  tapFun:function(e){
+    wx.vibrateShort()
+    this.setData({
+      chooseOne:e.currentTarget.dataset.count
+    })
+  }
 })
